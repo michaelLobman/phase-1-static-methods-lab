e27b826d0c2d0fac84c962a246f6c34714851d92
Michael Lobman <michael.lobman@gmail.com>
2021-11-18 22:33:13 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+
+    return string.charAt(0).toUpperCase() + string.slice(1);
+
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-' " "]+/g, "")
+  }
+  
+  static titleize(string){
+    const arr = string.split(" ");
+    const newArr = [];
+    const excluded = ['a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from', 'the'];
+
+    arr.map((word) => {
+      if (excluded.includes(word) !== true || word === arr[0]) {
+        newArr.push(word.charAt(0).toUpperCase() + word.slice(1));
+      } else {
+        newArr.push(word);
+      }
+
+    })
+
+    return newArr.join(" ");
+
+  }
+
 }
